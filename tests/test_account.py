30ad03254251f6fb3e3168fb300ab2e2e847183e
Joanne Tong <cheggmate@gmail.com>
2017-02-26 22:47:04 +0800

    Add accounts tests

--- tests/test_account.py
+++ tests/test_account.py
@@ -33,3 +33,24 @@ class TestAccount(TestCase):
         Account.get_default_account()
 
         account_retrieve_mock.assert_called_once_with(api_key=settings.STRIPE_SECRET_KEY)
+
+    @patch("stripe.Account.retrieve")
+    def test_account_delete_raises_unexpected_exception(self, account_retrieve_mock):
+        account_retrieve_mock.side_effect = stripe.InvalidRequestError("Unexpected Exception", "blah")
+
+        with self.assertRaisesMessage(stripe.InvalidRequestError, "Unexpected Exception"):
+            self.account.purge()
+
+        account_retrieve_mock.assert_called_once_with(self.account.stripe_id)
+
+    def test_transfer(self):
+        self.assertTrue(self.account.can_transfer())
+
+    @patch("stripe.Account.retrieve")
+    def test_cannot_transfer(self, acccount_retrieve_fake):
+        self.account.delete()
+        self.assertFalse(self.account.can_transfer())
+
+    def test_transfer_accepts_only_decimals(self):
+        with self.assertRaises(ValueError):
+            self.account.transfer(10)
